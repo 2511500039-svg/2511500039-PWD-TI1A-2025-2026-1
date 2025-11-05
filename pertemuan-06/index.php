9675924869ad22fc95fe27491e18aefecd0ae218
unknown <2511500039@mahasiswa.atmaluhur.ac.id>
2025-11-06 04:19:40 +0700

    FINALLY TUGAS KE-6 PAK YOHANES

--- pertemuan-06/index.php
+++ pertemuan-06/index.php
@@ -170,6 +170,134 @@
       <p><strong>Motto Hidup:</strong> <?= $motto; ?></p>
     </section>
 
+<section id="ipk" style="background-color: white; padding: 30px; color: black;">
+    <h2 style="text-align: center;">Nilai Saya</h2>
+
+    <?php
+    $namaMatkul1 = "Algoritma dan Struktur Data";
+    $sksMatkul1 = 4;
+    $nilaiHadir1 = 90;
+    $nilaiTugas1 = 60;
+    $nilaiUTS1 = 80;
+    $nilaiUAS1 = 70;
+
+    $namaMatkul2 = "Agama";
+    $sksMatkul2 = 2;
+    $nilaiHadir2 = 70;
+    $nilaiTugas2 = 50;
+    $nilaiUTS2 = 60;
+    $nilaiUAS2 = 80;
+
+    $namaMatkul3 = "Bahasa Indonesia";
+    $sksMatkul3 = 3;
+    $nilaiHadir3 = 85;
+    $nilaiTugas3 = 75;
+    $nilaiUTS3 = 70;
+    $nilaiUAS3 = 90;
+
+    $namaMatkul4 = "Sistem Operasi";
+    $sksMatkul4 = 4;
+    $nilaiHadir4 = 80;
+    $nilaiTugas4 = 80;
+    $nilaiUTS4 = 85;
+    $nilaiUAS4 = 75;
+
+    $namaMatkul5 = "Pemrograman Web Dasar";
+    $sksMatkul5 = 3;
+    $nilaiHadir5 = 69;
+    $nilaiTugas5 = 80;
+    $nilaiUTS5 = 90;
+    $nilaiUAS5 = 100;
+
+    function hitungNilai($hadir, $tugas, $uts, $uas, $sks)
+    {
+        $nilaiAkhir = (0.1 * $hadir) + (0.2 * $tugas) + (0.3 * $uts) + (0.4 * $uas);
+
+        if ($hadir < 70) {
+            $grade = "E";
+            $mutu = 0.00;
+        } elseif ($nilaiAkhir >= 85) {
+            $grade = "A";
+            $mutu = 4.00;
+        } elseif ($nilaiAkhir >= 80) {
+            $grade = "A-";
+            $mutu = 3.70;
+        } elseif ($nilaiAkhir >= 75) {
+            $grade = "B+";
+            $mutu = 3.30;
+        } elseif ($nilaiAkhir >= 70) {
+            $grade = "B";
+            $mutu = 3.00;
+        } elseif ($nilaiAkhir >= 65) {
+            $grade = "B-";
+            $mutu = 2.70;
+        } elseif ($nilaiAkhir >= 60) {
+            $grade = "C+";
+            $mutu = 2.30;
+        } elseif ($nilaiAkhir >= 55) {
+            $grade = "C";
+            $mutu = 2.00;
+        } elseif ($nilaiAkhir >= 50) {
+            $grade = "C-";
+            $mutu = 1.70;
+        } elseif ($nilaiAkhir >= 40) {
+            $grade = "D";
+            $mutu = 1.00;
+        } else {
+            $grade = "E";
+            $mutu = 0.00;
+        }
+
+        $bobot = $mutu * $sks;
+        $status = ($grade == "D" || $grade == "E") ? "GAGAL" : "LULUS";
+
+        return [$nilaiAkhir, $grade, $mutu, $bobot, $status];
+    }
+
+    list($nilaiAkhir1, $grade1, $mutu1, $bobot1, $status1) = hitungNilai($nilaiHadir1, $nilaiTugas1, $nilaiUTS1, $nilaiUAS1, $sksMatkul1);
+    list($nilaiAkhir2, $grade2, $mutu2, $bobot2, $status2) = hitungNilai($nilaiHadir2, $nilaiTugas2, $nilaiUTS2, $nilaiUAS2, $sksMatkul2);
+    list($nilaiAkhir3, $grade3, $mutu3, $bobot3, $status3) = hitungNilai($nilaiHadir3, $nilaiTugas3, $nilaiUTS3, $nilaiUAS3, $sksMatkul3);
+    list($nilaiAkhir4, $grade4, $mutu4, $bobot4, $status4) = hitungNilai($nilaiHadir4, $nilaiTugas4, $nilaiUTS4, $nilaiUAS4, $sksMatkul4);
+    list($nilaiAkhir5, $grade5, $mutu5, $bobot5, $status5) = hitungNilai($nilaiHadir5, $nilaiTugas5, $nilaiUTS5, $nilaiUAS5, $sksMatkul5);
+
+    $totalBobot = $bobot1 + $bobot2 + $bobot3 + $bobot4 + $bobot5;
+    $totalSKS = $sksMatkul1 + $sksMatkul2 + $sksMatkul3 + $sksMatkul4 + $sksMatkul5;
+    $IPK = $totalBobot / $totalSKS;
+
+    function tampilTabel($no, $nama, $sks, $hadir, $tugas, $uts, $uas, $akhir, $grade, $mutu, $bobot, $status)
+    {
+        echo "<table border='1' cellpadding='8' cellspacing='0' width='60%' style='margin: 20px auto; border-collapse: collapse;'>";
+        echo "<tr><th colspan='2' style='text-align:center;'>Nama Mata Kuliah ke-$no</th></tr>";
+        echo "<tr><td>Nama Matakuliah</td><td>$nama</td></tr>";
+        echo "<tr><td>SKS</td><td>$sks</td></tr>";
+        echo "<tr><td>Kehadiran</td><td>$hadir</td></tr>";
+        echo "<tr><td>Tugas</td><td>$tugas</td></tr>";
+        echo "<tr><td>UTS</td><td>$uts</td></tr>";
+        echo "<tr><td>UAS</td><td>$uas</td></tr>";
+        echo "<tr><td>Nilai Akhir</td><td>$akhir</td></tr>";
+        echo "<tr><td>Grade</td><td>$grade</td></tr>";
+        echo "<tr><td>Angka Mutu</td><td>$mutu</td></tr>";
+        echo "<tr><td>Bobot</td><td>$bobot</td></tr>";
+        echo "<tr><td>Status</td><td>$status</td></tr>";
+        echo "</table>";
+    }
+
+    tampilTabel(1, $namaMatkul1, $sksMatkul1, $nilaiHadir1, $nilaiTugas1, $nilaiUTS1, $nilaiUAS1, $nilaiAkhir1, $grade1, $mutu1, $bobot1, $status1);
+    tampilTabel(2, $namaMatkul2, $sksMatkul2, $nilaiHadir2, $nilaiTugas2, $nilaiUTS2, $nilaiUAS2, $nilaiAkhir2, $grade2, $mutu2, $bobot2, $status2);
+    tampilTabel(3, $namaMatkul3, $sksMatkul3, $nilaiHadir3, $nilaiTugas3, $nilaiUTS3, $nilaiUAS3, $nilaiAkhir3, $grade3, $mutu3, $bobot3, $status3);
+    tampilTabel(4, $namaMatkul4, $sksMatkul4, $nilaiHadir4, $nilaiTugas4, $nilaiUTS4, $nilaiUAS4, $nilaiAkhir4, $grade4, $mutu4, $bobot4, $status4);
+    tampilTabel(5, $namaMatkul5, $sksMatkul5, $nilaiHadir5, $nilaiTugas5, $nilaiUTS5, $nilaiUAS5, $nilaiAkhir5, $grade5, $mutu5, $bobot5, $status5);
+
+    echo "<table border='1' cellpadding='8' cellspacing='0' width='60%' style='margin: 20px auto; border-collapse: collapse; font-weight: bold;'>";
+    echo "<tr><th colspan='2' style='text-align:center;'>Rekapitulasi Nilai Akhir</th></tr>";
+    echo "<tr><td>Total Bobot</td><td>$totalBobot</td></tr>";
+    echo "<tr><td>Total SKS</td><td>$totalSKS</td></tr>";
+    echo "<tr><td>IPK</td><td>" . number_format($IPK, 2) . "</td></tr>";
+    echo "</table>";
+    ?>
+</section>
+
+
     <section id="contact">
       <h2>Kontak Kami</h2>
       <form action="" method="GET">
