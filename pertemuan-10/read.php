62a494572ae469065383db6f3a7ca0c64065eeb9
2511500039-svg <2511500039@mahasiswa.atmaluhur.ac.id>
2025-12-03 12:10:50 +0700

    WeoKu

--- pertemuan-10/read.php
+++ pertemuan-10/read.php
@@ -12,4 +12,12 @@ $q  = mysqli_query($conn_ $sql);
         <th>Pesan</th>
 </tr>
 
-<?php while ($row = mysqli_fetch_assoc($q)): s?>
+<?php while ($row = mysqli_fetch_assoc($q)): ?>
+    <tr>
+        <td><?= $row['cid']; ?></td>
+        <td><?= htmlspecialchars($row['cnama']); ?></td>
+        <td><?= htmlspecialchars($row['cemail']); ?></td>
+        <td><?= nl2br(htmlspecialchars($row['cpesan'])); ?></td>
+    </tr> 
+   <?php endwhile; ?>
+</table>
