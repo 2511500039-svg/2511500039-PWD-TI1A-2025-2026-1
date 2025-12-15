13e13e6de2573c8b74b3d3a60c2db932bf97c7c2
unknown <2511500039@mahasiswa.atmaluhur.ac.id>
2025-12-15 23:58:59 +0700

    wowowow.

--- pertemuan-11/read.php
+++ pertemuan-11/read.php
@@ -1,24 +1,28 @@
 <?php
 require 'koneksi.php';
 
-$sql =  "SELECT * FROM `tbl_tamu` ORDER BY cid DESC";
-$q  = mysqli_query($conn, $sql);
+$sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
+$q = mysqli_query($conn, $sql);
 if (!$q) die("Query Error: " . mysqli_error($conn));
 ?>
+
 <table border="1" cellpadding="8" cellspacing="0">
     <tr>
+        <th>No</th>
         <th>ID</th>
         <th>Nama</th>
         <th>Email</th>
         <th>Pesan</th>
     </tr>
 
+<?php $no = 1; ?>
 <?php while ($row = mysqli_fetch_assoc($q)): ?>
     <tr>
+        <td><?= $no++; ?></td>
         <td><?= $row['cid']; ?></td>
         <td><?= htmlspecialchars($row['cnama']); ?></td>
         <td><?= htmlspecialchars($row['cemail']); ?></td>
         <td><?= nl2br(htmlspecialchars($row['cpesan'])); ?></td>
-    </tr> 
+    </tr>
 <?php endwhile; ?>
 </table>
