7486bd16403cf49f847b7aae0bd117ac6dfd3080
unknown <2511500039@mahasiswa.atmaluhur.ac.id>
2025-12-24 11:54:48 +0700

    menghapus beberapa kodingan yang tidak diperlukan yang diambil dari prosesupdate.php di prosesdelete.php

--- pertemuan-13/proses_delete.php
+++ pertemuan-13/proses_delete.php
@@ -21,43 +21,26 @@ require_once __DIR__ . '/fungsi.php';
 
 
 
-$errors = [];
-
-
-
-
-if (!empty($errors)) {
-    $_SESSION['old'] = [
-        'nama'  => $nama,
-        'email' => $email,
-        'pesan' => $pesan
-    ];
-
-    $_SESSION['flash_error'] = implode('<br>', $errors);
-    redirect_ke('edit.php?cid=' . (int)$cid);
-}
 
 $stmt = mysqli_prepare(
     $conn,
-    "UPDATE tbl_tamu
-     SET cnama = ?, cemail = ?, cpesan = ?
-     WHERE cid = ?"
-);
+    "DELETE tbl_tamu");
+     #WHERE cid = ?");
 
 if (!$stmt) {
     $_SESSION['flash_error'] = 'Terjadi kesalahan sistem (prepare gagal).';
-    redirect_ke('edit.php?cid=' . (int)$cid);
+    redirect_ke('read.php');
 }
 
-mysqli_stmt_bind_param($stmt, "sssi", $nama, $email, $pesan, $cid);
+#mysqli_stmt_bind_param($stmt, "sssi", $nama, $email, $pesan, $cid);
 
 if (mysqli_stmt_execute($stmt)) {
-    unset($_SESSION['old']);
-    $_SESSION['flash_sukses'] = 'Terima kasih, data anda telah berhasil diperbarui.';
-    redirect_ke('read.php');
+    
+    $_SESSION['flash_sukses'] = 'Terima kasih, data anda telah berhasil dihapus.';
 } else {
-    $_SESSION['flash_error'] = 'Data gagal diperbarui. Silakan coba lagi.';
-    redirect_ke('edit.php?cid=' . (int)$cid);
+    $_SESSION['flash_error'] = 'Data gagal dihapus. Silakan coba lagi.';
 }
 
 mysqli_stmt_close($stmt);
+
+redirect_ke('read.php');
