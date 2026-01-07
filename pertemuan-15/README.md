11c06811a04100ac2be7891a055a01427cb79523
unknown <2511500039@mahasiswa.atmaluhur.ac.id>
2026-01-07 22:14:34 +0700

    Wowow

--- pertemuan-15/README.md
+++ pertemuan-15/README.md
@@ -1,1 +1,15 @@
-# pertemuan-15
+# Pertemuan 15
+
+Hari ini, Rabu 7.jan.2026, saya akan melakukan tryout:<br>
+NIM: 2511500039<br>
+Nama: Muhammad Alkautsar Dirgantara<br>
+Kelompok: TI1A<br>
+<br>
+Proses saya mengerjakan latihan ini sebagai berikut:<br>
+
+<ol>
+    <li>Mengesktrak bahan uas tryout</li>
+    <li>Membuat tabel baru di sql dengan nama tbl_biodata</li>
+    <li>Menambah beberapa kodingan file</li>
+    <li>menambah link delete di read.php untuk ke file proses_delete.php</li>
+</ol>
--- pertemuan-15/read_biodata.php
+++ pertemuan-15/read_biodata.php
@@ -1,46 +1,46 @@
-<?php
-session_start();
-require 'koneksi.php';
+    <?php
+    session_start();
+    require 'koneksi.php';
 
-$sql = "SELECT id, nim, nama, email, pesan, tanggal
-        FROM tbl_biodata
-        ORDER BY id DESC";
-$q = mysqli_query($conn, $sql);
+    $sql = "SELECT id, nim, nama, email, pesan, tanggal
+            FROM tbl_biodata
+            ORDER BY id DESC";
+    $q = mysqli_query($conn, $sql);
 
-if (!$q) {
-  die("Query error: " . mysqli_error($conn));
-}
-?>
+    if (!$q) {
+    die("Query error: " . mysqli_error($conn));
+    }
+    ?>
 
-<table border="1" cellpadding="8" cellspacing="0">
-  <tr>
-    <th>No</th>
-    <th>Aksi</th>
-    <th>ID</th>
-    <th>NIM</th>
-    <th>Nama</th>
-    <th>Email</th>
-    <th>Pesan</th>
-    <th>Tanggal</th>
-  </tr>
-
-  <?php $no = 1; ?>
-  <?php while ($row = mysqli_fetch_assoc($q)): ?>
+    <table border="1" cellpadding="8" cellspacing="0">
     <tr>
-      <td><?= $no++ ?></td>
-      <td>
-        <a href="edit_biodata.php?id=<?= (int)$row['id']; ?>">Edit</a> |
-        <a href="delete_biodata.php?id=<?= (int)$row['id']; ?>"
-           onclick="return confirm('Yakin hapus data ini?')">
-           Delete
-        </a>
-      </td>
-      <td><?= $row['id']; ?></td>
-      <td><?= htmlspecialchars($row['nim']); ?></td>
-      <td><?= htmlspecialchars($row['nama']); ?></td>
-      <td><?= htmlspecialchars($row['email']); ?></td>
-      <td><?= htmlspecialchars($row['pesan']); ?></td>
-      <td><?= $row['tanggal']; ?></td>
+        <th>No</th>
+        <th>Aksi</th>
+        <th>ID</th>
+        <th>NIM</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Pesan</th>
+        <th>Tanggal</th>
     </tr>
-  <?php endwhile; ?>
-</table>
+
+    <?php $no = 1; ?>
+    <?php while ($row = mysqli_fetch_assoc($q)): ?>
+        <tr>
+        <td><?= $no++ ?></td>
+        <td>
+            <a href="edit_biodata.php?id=<?= (int)$row['id']; ?>">Edit</a> |
+            <a href="delete_biodata.php?id=<?= (int)$row['id']; ?>"
+            onclick="return confirm('Yakin hapus data ini?')">
+            Delete
+            </a>
+        </td>
+        <td><?= $row['id']; ?></td>
+        <td><?= htmlspecialchars($row['nim']); ?></td>
+        <td><?= htmlspecialchars($row['nama']); ?></td>
+        <td><?= htmlspecialchars($row['email']); ?></td>
+        <td><?= htmlspecialchars($row['pesan']); ?></td>
+        <td><?= $row['tanggal']; ?></td>
+        </tr>
+    <?php endwhile; ?>
+    </table>
