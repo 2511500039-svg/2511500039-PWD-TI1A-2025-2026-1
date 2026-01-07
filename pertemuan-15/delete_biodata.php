01e90e677dad7d283d6e45e03659d4f01177fb97
unknown <2511500039@mahasiswa.atmaluhur.ac.id>
2026-01-07 22:49:50 +0700

    Wowow

--- pertemuan-15/delete_biodata.php
+++ pertemuan-15/delete_biodata.php
@@ -3,21 +3,21 @@ session_start();
 require 'koneksi.php';
 require_once __DIR__ . '/fungsi.php';
 
-# validasi parameter id
-$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT, [
+  'options' => ['min_range' => 1]
+]);
 
 if (!$id) {
   $_SESSION['flash_error'] = 'ID tidak valid.';
-  redirect_ke('index.php#contact');
+  redirect_ke('read_biodata.php');
 }
 
-# siapkan query DELETE dengan prepared statement
 $sql = "DELETE FROM tbl_biodata WHERE id = ?";
 $stmt = mysqli_prepare($conn, $sql);
 
 if (!$stmt) {
-  $_SESSION['flash_error'] = 'Terjadi kesalahan sistem (prepare gagal).';
-  redirect_ke('index.php#contact');
+  $_SESSION['flash_error'] = 'Terjadi kesalahan sistem.';
+  redirect_ke('read_biodata.php');
 }
 
 mysqli_stmt_bind_param($stmt, "i", $id);
@@ -29,4 +29,4 @@ if (mysqli_stmt_execute($stmt)) {
 }
 
 mysqli_stmt_close($stmt);
-redirect_ke('index.php#contact');
+redirect_ke('read_biodata.php');
