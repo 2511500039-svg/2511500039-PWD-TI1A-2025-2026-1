bd45deafcb5a2b4680edd3251157ccb4154b928f
unknown <2511500039@mahasiswa.atmaluhur.ac.id>
2026-01-28 11:11:02 +0700

    Wow

--- pertemuan-16/read.php
+++ pertemuan-16/read.php
@@ -2,44 +2,71 @@
 session_start();
 require 'koneksi.php';
 require 'fungsi.php';
-
-$sql = "SELECT * FROM tbl_biodata_dosen ORDER BY kode_dosen DESC";
-$q = mysqli_query($conn, $sql);
-
-if (!$q) {
-  die("Query error: " . mysqli_error($conn));
-}
 ?>
 
+<!-- ===================== -->
+<!-- TABEL BIODATA DOSEN -->
+<!-- ===================== -->
+<?php
+$sqlBio = "SELECT * FROM tbl_biodata_dosen ORDER BY id_dosen DESC";
+$qBio = mysqli_query($conn, $sqlBio);
+if (!$qBio) die(mysqli_error($conn));
+?>
+
+<h3>Data Biodata Dosen</h3>
 <table border="1" cellpadding="8" cellspacing="0">
   <tr>
     <th>No</th>
     <th>Kode Dosen</th>
-    <th>Nama Dosen</th>
-    <th>Alamat</th>
-    <th>Tanggal Jadi Dosen</th>
-    <th>JJA</th>
+    <th>Nama</th>
     <th>Prodi</th>
     <th>No HP</th>
-    <th>Nama Pasangan</th>
-    <th>Nama Anak</th>
-    <th>Bidang Ilmu</th>
   </tr>
+  <?php $i=1; while($b=mysqli_fetch_assoc($qBio)): ?>
+  <tr>
+    <td><?= $i++ ?></td>
+    <td><?= htmlspecialchars($b['kode_dosen']) ?></td>
+    <td><?= htmlspecialchars($b['nama_dosen']) ?></td>
+    <td><?= htmlspecialchars($b['prodi']) ?></td>
+    <td><?= htmlspecialchars($b['no_hp']) ?></td>
+  </tr>
+  <?php endwhile; ?>
+</table>
 
-  <?php $no = 1; ?>
-  <?php while ($row = mysqli_fetch_assoc($q)): ?>
-    <tr>
-      <td><?= $no++ ?></td>
-      <td><?= htmlspecialchars($row['kode_dosen']) ?></td>
-      <td><?= htmlspecialchars($row['nama_dosen']) ?></td>
-      <td><?= htmlspecialchars($row['alamat']) ?></td>
-      <td><?= htmlspecialchars($row['tgl_jadi_dosen']) ?></td>
-      <td><?= htmlspecialchars($row['jja']) ?></td>
-      <td><?= htmlspecialchars($row['prodi']) ?></td>
-      <td><?= htmlspecialchars($row['no_hp']) ?></td>
-      <td><?= htmlspecialchars($row['nama_pasangan']) ?></td>
-      <td><?= htmlspecialchars($row['nama_anak']) ?></td>
-      <td><?= htmlspecialchars($row['bidang_ilmu']) ?></td>
-    </tr>
+<br><hr><br>
+
+<!-- ===================== -->
+<!-- TABEL TAMU (KODE LAMA) -->
+<!-- ===================== -->
+<?php
+$sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
+$q = mysqli_query($conn, $sql);
+if (!$q) die(mysqli_error($conn));
+?>
+
+<h3>Data Tamu</h3>
+<table border="1" cellpadding="8" cellspacing="0">
+  <tr>
+    <th>No</th>
+    <th>Aksi</th>
+    <th>ID</th>
+    <th>Nama</th>
+    <th>Email</th>
+    <th>Pesan</th>
+    <th>Created At</th>
+  </tr>
+  <?php $i=1; while($row=mysqli_fetch_assoc($q)): ?>
+  <tr>
+    <td><?= $i++ ?></td>
+    <td>
+      <a href="edit.php?cid=<?= (int)$row['cid'] ?>">Edit</a>
+      <a onclick="return confirm('Hapus data?')" href="proses_delete.php?cid=<?= (int)$row['cid'] ?>">Delete</a>
+    </td>
+    <td><?= $row['cid'] ?></td>
+    <td><?= htmlspecialchars($row['cnama']) ?></td>
+    <td><?= htmlspecialchars($row['cemail']) ?></td>
+    <td><?= nl2br(htmlspecialchars($row['cpesan'])) ?></td>
+    <td><?= formatTanggal($row['dcreated_at']) ?></td>
+  </tr>
   <?php endwhile; ?>
 </table>
